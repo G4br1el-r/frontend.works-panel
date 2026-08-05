8c761f3a13b06400abf8e6b0e6d9767fd1a22cf9
Gabriel <gabriel_rodrigues_@hotmail.com>
2026-08-04 21:19:13 -0300

    adicionado speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Archivo, Archivo_Black } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { Header } from "@/components/landingpage/header";
 import {
@@ -108,6 +109,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-black font-sans">
         <Header />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
